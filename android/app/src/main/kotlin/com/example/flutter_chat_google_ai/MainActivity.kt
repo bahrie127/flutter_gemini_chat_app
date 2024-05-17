7ee0f94b214c33967efd637f1f7a7a2b88a388eb
bahri <saiful.bahri.tl@gmail.com>
2024-05-17 17:05:54 +0700

    free event cwb google generative ai flutter

---
+++ android/app/src/main/kotlin/com/example/flutter_chat_google_ai/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_chat_google_ai
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/chat_page.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+
+import 'widgets/chat_bubble.dart';
+
+const apiKey = 'AIzaSyAsM7WdFtuMOhvwuODMOUrtFBq2dXIFl1M';
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({super.key});
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  final model = GenerativeModel(model: 'gemini-pro', apiKey: apiKey);
+
+  TextEditingController messageController = TextEditingController();
+
+  bool isLoading = false;
+
+  List<ChatBubble> chatBubbles = [
+    const ChatBubble(
+      direction: Direction.left,
+      message: 'Halo, saya GEMINI AI. Ada yang bisa saya bantu?',
+      photoUrl: 'https://i.pravatar.cc/150?img=47',
+      type: BubbleType.alone,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: CupertinoButton(
+          child: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+          ),
+          onPressed: () => Navigator.pop(context),
+        ),
+        title: const Text('Gemini Personal Assistant AI',
+            style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              physics: const AlwaysScrollableScrollPhysics(
+                  parent: BouncingScrollPhysics()),
+              reverse: true,
+              padding: const EdgeInsets.all(10),
+              children: chatBubbles.reversed.toList(),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              children: [
+                 Expanded(
+                  child: TextField(
+                    controller: messageController,
+                    decoration: const InputDecoration(
+                      hintText: 'Type a message...',
+                    ),
+                  ),
+                ),
+                isLoading
+                    ? const CircularProgressIndicator.adaptive()
+                    : IconButton(
+                        icon: const Icon(Icons.send),
+                        onPressed: () async {
+                          // Send message logic here
+                          setState(() {
+                            isLoading = true;
+                          });
+                          final content = [
+                            Content.text(messageController.text)
+                          ];
+                          final GenerateContentResponse responseAI =
+                              await model.generateContent(content);
+
+                          chatBubbles = [
+                            ...chatBubbles,
+                            ChatBubble(
+                              direction: Direction.right,
+                              message: messageController.text,
+                              photoUrl: null,
+                              type: BubbleType.alone,
+                            )
+                          ];
+
+                          chatBubbles = [
+                            ...chatBubbles,
+                            ChatBubble(
+                              direction: Direction.left,
+                              message: responseAI.text ??
+                                  'Maaf, saya tidak mengerti',
+                              photoUrl: 'https://i.pravatar.cc/150?img=47',
+                              type: BubbleType.alone,
+                            )
+                          ];
+
+                          messageController.clear();
+                          setState(() {
+                            isLoading = false;
+                          });
+                        },
+                      ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
